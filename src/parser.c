f3ae17733822ab83ffab330cc75fd149338c8dac
Masashi Umezawa <ume@softumeya.com>
2026-07-08 22:08:58 +0900

    Bump parser patch version to 0.2.4

--- src/parser.c
+++ src/parser.c
@@ -10011,7 +10011,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_tonel_smalltalk(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 2,
+      .patch_version = 4,
     },
   };
   return &language;
